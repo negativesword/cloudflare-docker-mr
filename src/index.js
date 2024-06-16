29ccc7430768c38dd03fd4d49258c7049402a04d
negativesword <46472575+negativesword@users.noreply.github.com>
2024-06-16 21:30:34 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.cloudmate.uk": "https://registry-1.docker.io",
+  "quay.cloudmate.uk": "https://quay.io",
+  "gcr.cloudmate.uk": "https://gcr.io",
+  "k8s-gcr.cloudmate.uk": "https://k8s.gcr.io",
+  "k8s.cloudmate.uk": "https://registry.k8s.io",
+  "ghcr.cloudmate.uk": "https://ghcr.io",
+  "cloudsmith.cloudmate.uk": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.cloudmate.uk": dockerHub,
 };
 
 function routeByHosts(host) {
